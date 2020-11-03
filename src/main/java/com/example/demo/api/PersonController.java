1cbf918b598cb413a2d4375bc22f8e274ce8457f
Tim Dinh <timdinh@192-168-1-110.tpgi.com.au>
2020-11-03 22:36:58 +1100

    init wip

---
+++ src/main/java/com/example/demo/api/PersonController.java
@@ -0,0 +1,37 @@
+package com.example.demo.api;
+
+import com.example.demo.model.Person;
+import com.example.demo.service.PersonService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+@RequestMapping("api/v1/person")
+@RestController
+public class PersonController {
+
+    private PersonService personService;
+
+    @Autowired
+    public PersonController(PersonService personService) {
+        this.personService = personService;
+    }
+
+    @PostMapping
+    public void addPerson(@RequestBody Person person) {
+        personService.addPerson(person);
+    }
+
+    @GetMapping
+    public List<Person> selectAllPeople() {
+        return personService.getAllPeople();
+    }
+
+    @GetMapping(path = "{id}")
+    public Person selectPersonById (@PathVariable("id") UUID id) {
+        return personService.getPersonById(id).orElse(null);
+    }
+}
